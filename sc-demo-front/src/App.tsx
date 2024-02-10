ef4540739ec977e8080eee63ca3037b58d3f9157
akanation4 <103402576+akanation4@users.noreply.github.com>
2024-02-10 22:52:45 +0900

    first commit

---
+++ sc-demo-front/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+interface DataResponse {
+    Hello?: string;
+}
+
+const App: React.FC = () => {
+    const [data, setData] = useState<DataResponse | null>(null);
+    const url = "http://127.0.0.1:8000";
+
+    const GetData = () => {
+        axios.get<DataResponse>(url).then((res) => {
+            setData(res.data);
+        });
+    };
+
+    return (
+        <div>
+            <div>SC demo test</div>
+            {data ? <div>{data.Hello}</div> : <button onClick={GetData}>Get Data</button>}
+        </div>
+    );
+}
+
+export default App;
